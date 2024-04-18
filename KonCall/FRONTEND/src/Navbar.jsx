7a6a30ce992dbc183bc513a2c12a5c2e92b35268
PIYUSH <piyush.class@gmail.com>
2024-04-18 21:19:58 +0530

    box-icon added

--- KonCall/FRONTEND/src/Navbar.jsx
+++ KonCall/FRONTEND/src/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './app.scss';
+import 'boxicons'
+
 
 function Navbar() {
     return (
         
          <nav className="navbar" id="navbar">
-            <div className="navbar-heading">Kon<span>Call</span></div>
+            <div className="navbar-heading">
+                
+            Kon<span>Call</span></div>
             <div className="links-container">
            
                 <a href="#navbar" className="link">Home</a>
                 <a href="#navbar" className="link">About</a>
                 <a href="#navbar" className="link">Pages</a>
                 <a href="#navbar" className="link">Contact</a>
-                <button className="login-btn">SignIn / SignUp</button>
+                <button className="login-btn">SignUp</button>
+                <button className="login-btn">SignIn</button>
             </div>
         </nav>
     );
